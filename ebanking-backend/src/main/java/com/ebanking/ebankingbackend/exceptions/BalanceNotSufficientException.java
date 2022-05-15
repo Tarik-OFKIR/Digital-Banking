532ab1467e81132b08e16557885544f19a9016e0
Tarik-OFKIR <tarikofkir2017@gmail.com>
2022-05-15 21:38:54 +0100

    creating mappers & exceptions & BankAccountService

---
+++ ebanking-backend/src/main/java/com/ebanking/ebankingbackend/exceptions/BalanceNotSufficientException.java
@@ -0,0 +1,7 @@
+package com.ebanking.ebankingbackend.exceptions;
+
+public class BalanceNotSufficientException extends Exception {
+    public BalanceNotSufficientException(String message) {
+        super(message);
+    }
+}
